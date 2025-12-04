5806a664be3b8afd6bee31d84e973d8b2c0ea62e
Owner <fevzifd2023@gmail.com>
2025-12-04 16:27:03 -0500

    create Order class

---
+++ src/main/java/com/northwind/model/Order.java
@@ -0,0 +1,156 @@
+package com.northwind.model;
+
+
+import java.time.LocalDateTime;
+
+public class Order {
+
+    private int orderID;
+    private String customerID;
+    private int employeeID;
+    private LocalDateTime orderDate;
+    private LocalDateTime requiredDate;
+    private LocalDateTime shippedDate;
+    private int shipVia;
+    private double freight;
+    private String shipName;
+    private String shipAddress;
+    private String shipCity;
+    private String shipRegion;
+    private String shipPostalCode;
+    private String shipCountry;
+
+    public Order() {
+    }
+
+    public Order(int orderID, String customerID, int employeeID, LocalDateTime orderDate, LocalDateTime requiredDate,
+                 LocalDateTime shippedDate, int shipVia, double freight, String shipName, String shipAddress,
+                 String shipCity, String shipRegion, String shipPostalCode, String shipCountry) {
+        this.orderID = orderID;
+        this.customerID = customerID;
+        this.employeeID = employeeID;
+        this.orderDate = orderDate;
+        this.requiredDate = requiredDate;
+        this.shippedDate = shippedDate;
+        this.shipVia = shipVia;
+        this.freight = freight;
+        this.shipName = shipName;
+        this.shipAddress = shipAddress;
+        this.shipCity = shipCity;
+        this.shipRegion = shipRegion;
+        this.shipPostalCode = shipPostalCode;
+        this.shipCountry = shipCountry;
+    }
+
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(String customerID) {
+        this.customerID = customerID;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public LocalDateTime getRequiredDate() {
+        return requiredDate;
+    }
+
+    public void setRequiredDate(LocalDateTime requiredDate) {
+        this.requiredDate = requiredDate;
+    }
+
+    public LocalDateTime getShippedDate() {
+        return shippedDate;
+    }
+
+    public void setShippedDate(LocalDateTime shippedDate) {
+        this.shippedDate = shippedDate;
+    }
+
+    public int getShipVia() {
+        return shipVia;
+    }
+
+    public void setShipVia(int shipVia) {
+        this.shipVia = shipVia;
+    }
+
+    public double getFreight() {
+        return freight;
+    }
+
+    public void setFreight(double freight) {
+        this.freight = freight;
+    }
+
+    public String getShipName() {
+        return shipName;
+    }
+
+    public void setShipName(String shipName) {
+        this.shipName = shipName;
+    }
+
+    public String getShipAddress() {
+        return shipAddress;
+    }
+
+    public void setShipAddress(String shipAddress) {
+        this.shipAddress = shipAddress;
+    }
+
+    public String getShipCity() {
+        return shipCity;
+    }
+
+    public void setShipCity(String shipCity) {
+        this.shipCity = shipCity;
+    }
+
+    public String getShipRegion() {
+        return shipRegion;
+    }
+
+    public void setShipRegion(String shipRegion) {
+        this.shipRegion = shipRegion;
+    }
+
+    public String getShipPostalCode() {
+        return shipPostalCode;
+    }
+
+    public void setShipPostalCode(String shipPostalCode) {
+        this.shipPostalCode = shipPostalCode;
+    }
+
+    public String getShipCountry() {
+        return shipCountry;
+    }
+
+    public void setShipCountry(String shipCountry) {
+        this.shipCountry = shipCountry;
+    }
+}
