4f3a9f2ebf98c1337a00ba3a88c985991fe56be9
Owner <fevzifd2023@gmail.com>
2025-12-04 16:07:55 -0500

    edit configuration

--- src/main/java/com/northwind/Main.java
+++ src/main/java/com/northwind/Main.java
@@ -1,8 +1,22 @@
 package com.northwind;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
 public class Main {
 
     public static void main(String[] args) {
+        String url="jdbc:mysql://localhost:3306/northwind";
+        String username=args[0];
+        String password=args[1];
+
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+
+
+
 
     }
 
